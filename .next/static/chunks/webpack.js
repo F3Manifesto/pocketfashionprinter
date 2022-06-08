9bb7adf925e1a6934a8ba16b2ce701af5ac07167
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-06-08 17:40:11 -0400

    fix text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c03b0d42e292b49b233e";
+/******/ 	var hotCurrentHash = "0389315aa74f98afda60";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/homepage/CollectionSection/index.js
+++ src/components/homepage/CollectionSection/index.js
@@ -5,12 +5,12 @@ const CollectionSection = () => {
   return (
     <div className={styles.collectionWrapper}>
       <h1>
-        Composable closet collections
+        Composable Closet Collections
       </h1>
       
       <div className={styles.bottomPart}>
         <div className={styles.description}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Stock your inventory with the latest NFT drip, digital and physical. Using FGO from DIGITALAX.
         </div>
         <img src='/images/homepage/closetfemale.gif' />
       </div>
