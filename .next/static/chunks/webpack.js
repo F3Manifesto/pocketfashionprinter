c2e72bb11f1bdcb66f63bb9538af6089d9b2963b
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-06-08 20:18:26 -0400

    fix mobile version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0389315aa74f98afda60";
+/******/ 	var hotCurrentHash = "c8b320f51d2b8a5afca5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
